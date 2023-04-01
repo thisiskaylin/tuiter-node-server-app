c8b4399ba97f866edd33d14e7fb6909adda8ba4b
Kaylin Lau <lau.kay@northeastern.edu>
2023-03-31 23:28:32 -0700

    Mongo server update

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -4,5 +4,15 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  topic: String,
+  title: String,
+  username: String,
+  handle: String,
+  time: String,
+  image: String,
+  replies: Number,
+  retuits: Number,
+  disliked: Boolean,
+  dislikes: Number
 }, {collection: 'tuits'});
 export default schema;
